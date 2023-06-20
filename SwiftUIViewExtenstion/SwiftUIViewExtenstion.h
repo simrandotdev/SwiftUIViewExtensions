695110532d97de19590386ce38da7058ca7d54fc
Simran Preet Singh <simran5590@gmail.com>
2023-06-20 13:50:51 -0700

    Initial Commit

---
+++ SwiftUIViewExtenstion/SwiftUIViewExtenstion.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewExtenstion.h
+//  SwiftUIViewExtenstion
+//
+//  Created by Simran Preet Narang on 2023-06-20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIViewExtenstion.
+FOUNDATION_EXPORT double SwiftUIViewExtenstionVersionNumber;
+
+//! Project version string for SwiftUIViewExtenstion.
+FOUNDATION_EXPORT const unsigned char SwiftUIViewExtenstionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIViewExtenstion/PublicHeader.h>
+
+
